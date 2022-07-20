04dd819e0297e05a4b18ccc25070fca0100b77ae
Darina <darina.blazevica@accenture.com>
2022-07-20 11:38:07 +0300

    Develop branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from "../src/components/organisms/About/About"
+import structure from "./structure.json"
 
-function App() {
+const App: React.FC =() =>{
+
+  const about = structure.about;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About title={about.title} subtitle={about.subTitle} content={about.text} imageArr={about.aboutImages} altTag={about.aboutImages}></About>
     </div>
   );
 }
