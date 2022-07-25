36ae628c947fe6e3f5d95f6d453e551503dbc930
Darina <darina.blazevica@accenture.com>
2022-07-25 11:58:13 +0300

    Footer component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import About from "./components/organisms/About/About";
+import Footer from "./components/organisms/Footer/Footer";
 import Gallery from "./components/organisms/Gallery/Gallery";
 import structure from "./structure.json";
 
 const App = () => {
   const about = structure.about;
   const gallery = structure.gallery;
+  const footer = structure.footer;
 
   return (
     <div className="App">
@@ -20,6 +22,7 @@ const App = () => {
         subtitle={gallery.subTitle}
         galleryImages={gallery.galleryImages}
       ></Gallery>
+      <Footer followUs={footer.followUs} title={footer.title} copyright={footer.copyright} socialMedia={footer.socialMedia}></Footer>
     </div>
   );
 };
